45aaa7ed6aa14fcee9b30cf8faf9124f2d3aa0a5
Jbernat88 <91455587+Jbernat88@users.noreply.github.com>
2022-02-18 13:48:43 +0100

    ssssssss

--- Assets/Entregable 7/Scripts/PlayerController.cs
+++ Assets/Entregable 7/Scripts/PlayerController.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float jumpForce = 8f;
+    private Rigidbody playerRigidbody;
+    private float yRange = 13.8f;
+    private float lowerlim = 0f;
+    private float impulse = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //El player salta con el espacio
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+       
         
+        if (transform.position.y >= yRange)
+        {
+            playerRigidbody.AddForce(Vector3.down * impulse, ForceMode.Impulse);
+        }
+
+        
+        
+
+
     }
+
 }
